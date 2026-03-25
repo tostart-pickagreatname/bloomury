66acf801f1984cd50acbeb4326a66dbf685f0f35
@tostart-pickagreatname <steven.bradley.scott@gmail.com>
2026-03-25 01:15:32 -0700

    Add tests for argument validation and normal filter usage

--- test/test_bloomury.rb
+++ test/test_bloomury.rb
@@ -7,7 +7,65 @@ class TestBloomury < Minitest::Test
     refute_nil ::Bloomury::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  # --- argument validation ---
+
+  def test_negative_capacity_raises
+    assert_raises(ArgumentError) { Bloomury::Filter.new(-1, 0.01) }
+  end
+
+  def test_zero_capacity_raises
+    assert_raises(ArgumentError) { Bloomury::Filter.new(0, 0.01) }
+  end
+
+  def test_zero_error_rate_raises
+    assert_raises(ArgumentError) { Bloomury::Filter.new(1000, 0) }
+  end
+
+  def test_error_rate_of_one_raises
+    assert_raises(ArgumentError) { Bloomury::Filter.new(1000, 1) }
+  end
+
+  def test_error_rate_above_one_raises
+    assert_raises(ArgumentError) { Bloomury::Filter.new(1000, 1.5) }
+  end
+
+  def test_negative_error_rate_raises
+    assert_raises(ArgumentError) { Bloomury::Filter.new(1000, -0.01) }
+  end
+
+  def test_infeasible_allocation_raises
+    assert_raises(RangeError) { Bloomury::Filter.new(1e18, 1e-300) }
+  end
+
+  # --- normal usage ---
+
+  def test_include_returns_true_after_add
+    f = Bloomury::Filter.new(1000, 0.01)
+    f.add("hello")
+    assert f.include?("hello")
+  end
+
+  def test_include_returns_false_for_missing_item
+    f = Bloomury::Filter.new(1000, 0.01)
+    refute f.include?("hello")
+  end
+
+  def test_count_increments_on_add
+    f = Bloomury::Filter.new(1000, 0.01)
+    assert_equal 0, f.count
+    f.add("a")
+    assert_equal 1, f.count
+    f.add("b")
+    assert_equal 2, f.count
+  end
+
+  def test_bit_count_is_positive
+    f = Bloomury::Filter.new(1000, 0.01)
+    assert_operator f.bit_count, :>, 0
+  end
+
+  def test_hash_count_is_positive
+    f = Bloomury::Filter.new(1000, 0.01)
+    assert_operator f.hash_count, :>, 0
   end
 end
